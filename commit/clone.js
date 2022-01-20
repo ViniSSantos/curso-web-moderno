d3a9b0a65770d5a39fd5c9fec2a9c7b717229f90
Murilo Eduardo dos Santos <murilo.santos@somosphi.com>
2022-01-19 22:59:37 -0300

    add watermelon and newline of end file

--- commit/clone.js
+++ commit/clone.js
@@ -3,7 +3,7 @@ console.log('Testando commando git');
 // Teste de atividade com GitHub
 console.log('Junçãozinha 02 de ... SALVE');
 
-const fruts = [🍌, 🍓, 🍐];
+const fruts = [🍌, 🍓, 🍐, 🍉];
 
 for (let index = 0; index < fruts.length; index++) {
     const element = fruts[index];
