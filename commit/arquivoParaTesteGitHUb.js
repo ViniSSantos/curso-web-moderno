6193f58c58488910c432d286442d7b6bf2ef97ab
Murilo Eduardo dos Santos <murilo.santos@somosphi.com>
2022-01-19 22:42:08 -0300

    Add new file to tests GitHub and changes clone

---
+++ commit/arquivoParaTesteGitHUb.js
@@ -0,0 +1,9 @@
+const array = ['Maconha', 'cocaina', 'LSD'];
+
+(() => {
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+
+    console.log(element);
+  }
+})();
