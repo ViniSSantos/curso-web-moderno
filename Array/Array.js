625b925c594378eb7f5e264471d096590ab0afe8
ViniSSantos <cepef.rh@gmail.com>
2022-02-06 16:51:14 -0300

    Array - sort, splice

--- Array/Array.js
+++ Array/Array.js
@@ -9,3 +9,25 @@ console.log(aprovados[0])
 console.log(aprovados[1])
 console.log(aprovados[2])
 console.log(aprovados[3])
+
+aprovados[3] = 'Paulo'
+aprovados.push('Abia') // incluir novo na ultima posição do Array
+console.log(aprovados.length) // .legth é usado para medir o tamanho de indices do array
+
+aprovados[9] = 'Rafael'
+console.log(aprovados.length)
+console.log(aprovados[7] === null)
+console.log(aprovados[8] === undefined)
+
+console.log(aprovados)
+aprovados.sort() //.sort = Organiza em ordem alfabética
+console.log(aprovados)
+
+delete aprovados[1]
+console.log(aprovados[1])
+console.log(aprovados[2])
+
+aprovados = ['Bia', 'Carlos','Ana']
+aprovados.splice(1, 2, 'Elemento1', 'Elemento2' )// .splice( 1º indice(1) , 2º QuntsVezes Elimina(2), 3º adiciona a partir do indice 'Elemento' ) Adiciona e elimina elemntos em Array
+//aprovados.splice(1, 0, 'Elemento1') // Á partir do indice 1, não elimina nada, adiciona elemnto
+console.log(aprovados)
